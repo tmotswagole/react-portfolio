001e6722a038f7f78d7533803e3384317de10fb1
tmotswagole <thabiso10000@gmail.com>
2022-10-24 12:19:42 +0200

    added bubbles and cv

--- src/App.js
+++ src/App.js
@@ -10,10 +10,73 @@ import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import Scrollup from './components/scrollup/Scrollup';
 
-function App() {
+function App() {{
   return (
     <>
       <Header />
+
+      <div className='bubbles'>
+        <span style={{ '--i': "11" }}></span>
+        <span style={{ '--i': "12" }}></span>
+        <span style={{ '--i': "24" }}></span>
+        <span style={{ '--i': "10" }}></span>
+        <span style={{ '--i': "14" }}></span>
+        <span style={{ '--i': "23" }}></span>
+        <span style={{ '--i': "18" }}></span>
+        <span style={{ '--i': "16" }}></span>
+        <span style={{ '--i': "19" }}></span>
+        <span style={{ '--i': "20" }}></span>
+        <span style={{ '--i': "22" }}></span>
+        <span style={{ '--i': "25" }}></span>
+        <span style={{ '--i': "18" }}></span>
+        <span style={{ '--i': "21" }}></span>
+        <span style={{ '--i': "15" }}></span>
+        <span style={{ '--i': "13" }}></span>
+        <span style={{ '--i': "26" }}></span>
+        <span style={{ '--i': "17" }}></span>
+        <span style={{ '--i': "13" }}></span>
+        <span style={{ '--i': "28" }}></span>
+        <span style={{ '--i': "11" }}></span>
+        <span style={{ '--i': "12" }}></span>
+        <span style={{ '--i': "24" }}></span>
+        <span style={{ '--i': "10" }}></span>
+        <span style={{ '--i': "14" }}></span>
+        <span style={{ '--i': "23" }}></span>
+        <span style={{ '--i': "18" }}></span>
+        <span style={{ '--i': "16" }}></span>
+        <span style={{ '--i': "19" }}></span>
+        <span style={{ '--i': "20" }}></span>
+        <span style={{ '--i': "22" }}></span>
+        <span style={{ '--i': "25" }}></span>
+        <span style={{ '--i': "18" }}></span>
+        <span style={{ '--i': "21" }}></span>
+        <span style={{ '--i': "15" }}></span>
+        <span style={{ '--i': "13" }}></span>
+        <span style={{ '--i': "26" }}></span>
+        <span style={{ '--i': "17" }}></span>
+        <span style={{ '--i': "13" }}></span>
+        <span style={{ '--i': "28" }}></span>
+        <span style={{ '--i': "11" }}></span>
+        <span style={{ '--i': "12" }}></span>
+        <span style={{ '--i': "24" }}></span>
+        <span style={{ '--i': "10" }}></span>
+        <span style={{ '--i': "14" }}></span>
+        <span style={{ '--i': "23" }}></span>
+        <span style={{ '--i': "18" }}></span>
+        <span style={{ '--i': "16" }}></span>
+        <span style={{ '--i': "19" }}></span>
+        <span style={{ '--i': "20" }}></span>
+        <span style={{ '--i': "22" }}></span>
+        <span style={{ '--i': "25" }}></span>
+        <span style={{ '--i': "18" }}></span>
+        <span style={{ '--i': "21" }}></span>
+        <span style={{ '--i': "15" }}></span>
+        <span style={{ '--i': "13" }}></span>
+        <span style={{ '--i': "26" }}></span>
+        <span style={{ '--i': "17" }}></span>
+        <span style={{ '--i': "13" }}></span>
+        <span style={{ '--i': "28" }}></span>
+      </div>
 
       <main className='main'>
         <Home />
@@ -29,6 +92,6 @@ function App() {
 
     </>
   );
-}
+}}
 
 export default App;
