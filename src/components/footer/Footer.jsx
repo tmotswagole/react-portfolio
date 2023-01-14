fd0aa94bc0304575f4c188a35c0c4879aea1875b
tmotswagole <thabiso10000@gmail.com>
2023-01-14 15:37:14 +0200

    Added socials

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,72 +1,93 @@
-import React from 'react';
+import React from "react";
 import "./footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
-        
-        <div className="footer__container container">
+      <div className="footer__container container">
+        <h1 className="footer__title">Thabiso</h1>
 
-            <h1 className="footer__title">Thabiso</h1>
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
 
-            <ul className="footer__list">
+          <li>
+            <a href="#projects" className="footer__link">
+              Projects
+            </a>
+          </li>
 
-                <li>
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Portfolio
+            </a>
+          </li>
 
-                    <a href="#about" className="footer__link">About</a>
+          <li>
+            <a href="#contact" className="footer__link">
+              Contact
+            </a>
+          </li>
+        </ul>
 
-                </li>
+        {/*============================= Socials =============================*/}
 
-                <li>
+        <div className="footer__social">
+          <a
+            href="https://www.linkedin.com/in/thabiso-solomon-motswagole/"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-linkedin"></i>
+          </a>
 
-                    <a href="#projects" className="footer__link">Projects</a>
+          <a
+            href="https://github.com/tmotswagole"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-github"></i>
+          </a>
 
-                </li>
+          <a
+            href="https://www.instagram.com/wearethabiso/"
+            className="home__social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-instagram"></i>
+          </a>
 
-                <li>
+          <a
+            href="https://www.tiktok.com/@wearethabiso"
+            className="home__social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-tiktok"></i>
+          </a>
 
-                    <a href="#portfolio" className="footer__link">Portfolio</a>
-
-                </li>
-
-                <li>
-
-                    <a href="#contact" className="footer__link">Contact</a>
-
-                </li>
-
-            </ul>
-
-            {/*============================= Socials =============================*/}
-
-            <div className="footer__social">
-
-                <a href="https://www.linkedin.com/in/thabiso-solomon-motswagole/" className="footer__social-link" target="_blank" rel="noreferrer">
-
-                    <i className="uil uil-linkedin"></i>
-
-                </a>
-
-                <a href="https://github.com/tmotswagole" className="footer__social-link" target="_blank" rel="noreferrer">
-
-                    <i className="uil uil-github"></i>
-
-                </a>
-
-                <a href="https://www.instagram.com/rethabiso/" className="footer__social-link" target="_blank" rel="noreferrer">
-
-                    <i className="uil uil-instagram"></i>
-
-                </a>
-
-            </div>
-
-            <span className="footer__copy">&#169; Copyright. All rigths reserved</span>
-
+          <a
+            href="https://twitter.com/WeAreThabiso"
+            className="home__social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="uil uil-twitter"></i>
+          </a>
         </div>
 
+        <span className="footer__copy">
+          &#169; Copyright. All rigths reserved
+        </span>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,34 +1,58 @@
-import React from 'react'
-
+import React from "react";
 
 const Social = () => {
   return (
     <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/thabiso-solomon-motswagole/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
 
-        <a href="https://www.linkedin.com/in/thabiso-solomon-motswagole/" className="home__social-icon" target="_blank" rel="noreferrer">
+      <a
+        href="https://github.com/tmotswagole"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-github"></i>
+      </a>
 
-            <i className="uil uil-linkedin"></i>
+      <a
+        href="https://www.instagram.com/wearethabiso/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
 
-        </a>
+      <a
+        href="https://www.tiktok.com/@wearethabiso"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-tiktok"></i>
+      </a>
 
-        <a href="https://github.com/tmotswagole" className="home__social-icon" target="_blank" rel="noreferrer">
+      <a
+        href="https://twitter.com/WeAreThabiso"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-twitter"></i>
+      </a>
 
-            <i className="uil uil-github"></i>
-
-        </a>
-
-        <a href="https://www.instagram.com/rethabiso/" className="home__social-icon" target="_blank" rel="noreferrer">
-
-            <i className="uil uil-instagram"></i>
-
-        </a>
-
-        {/* <a href="" className="home__social-icon" target="_blank"></a> */}
-
+      {/* <a href="" className="home__social-icon" target="_blank"></a> */}
     </div>
-  )
-}
+  );
+};
 
-export default Social
+export default Social;
 
 // https://iconscout.com/unicons/explore/line
